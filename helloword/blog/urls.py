138b6ccf545a60d0b814cec596d2f46469c61f1f
megetzz <ma.1971250916@qq.com>
2020-03-17 18:12:51 +0800

    3

--- helloword/blog/urls.py
+++ helloword/blog/urls.py
@@ -3,5 +3,8 @@ from blog import views
 
 urlpatterns = [
     path('hello/',views.index),
-    path('show/',views.show_detail)
+    path('show/',views.show_detail),
+    path('show_article/',views.show_article),
+    path('show_article2/',views.show_article2),
+    path('show_articles/',views.show_articles),
 ]
--- helloword/blog/views.py
+++ helloword/blog/views.py
@@ -11,3 +11,20 @@ def show_detail(request):
     first_article=Article.objects.all()[0]
 
     return HttpResponse(first_article.title)
+
+# 一个
+def show_article(request):
+    first_article=Article.objects.all()[0]
+    return render(request,'show.html',{'article':first_article})
+
+# 两个
+def show_article2(request):
+    article=Article.objects.all()[0]
+    # first_article=Article.objects.all()[0]
+    first_article1=Article.objects.all()[1]
+    return render(request,'show2.html',{'article':article,'article1':first_article1})
+
+# 全部
+def show_articles(request):
+    articles=Article.objects.all()
+    return render(request,'shows.html',{'articles':articles})
